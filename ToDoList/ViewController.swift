6177d52c31a0b1abcb7d3351403ecfd2a90fb547
ILC-1988 <ilc198810@gmail.com>
2023-10-16 22:54:55 +0300

    first

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Илья Черницкий on 16.10.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
